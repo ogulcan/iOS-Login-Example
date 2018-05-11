5ba934218dee7ebf6b7cdcba85d94b24ac7832e5
Ogulcan <junkchorn@gmail.com>
2018-05-11 22:52:05 +0300

    Add splash view controller w/ colors

---
+++ Login/SplashViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SplashViewController.swift
+//  login
+//
+//  Created by Oğulcan on 11/05/2018.
+//  Copyright © 2018 ogulcan. All rights reserved.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
