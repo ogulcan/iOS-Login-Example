3446fd5fe80945a6a1e04fc4496e8b3e22416222
Ogulcan <junkchorn@gmail.com>
2018-05-11 23:15:05 +0300

    Add login view controller

---
+++ Login/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  login
+//
+//  Created by Oğulcan on 11/05/2018.
+//  Copyright © 2018 ogulcan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
